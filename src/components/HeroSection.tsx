2fa69eac850422e231b9e3795a9934fe32f627de
Kauscode-dev <kaustubhchauhan92@gmail.com>
2026-01-19 20:34:48 +0000

    commit commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -66,9 +66,6 @@ export const HeroSection = () => {
               View Our Process
             </Button>
           </motion.div>
-
-          {/* Bottom gradient fade */}
-          <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent" />
         </div>
       </div>
 
